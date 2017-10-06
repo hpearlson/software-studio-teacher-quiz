f63c6dc73614843d63a1310b42347eb87554e60f
msummer300 <msummer@tulane.edu>
2017-10-06 00:25:12 +0000

    started coding courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,20 @@
 class Course < ActiveRecord::Base
+    
+    def initialize(course_name, teacher_id=1, student_list=[])
+        @course_name = course_name
+        @teacher_id = teacher_id
+        @student_list = student_list
+    end
+    
+    def add_student(student_id)
+        @student_list.push(student_id)
+    end
+    
+    def del_student(student_id)
+       @student_list.delete(student_id) 
+    end
+    
+    def display_students
+        puts "Here is where the student will be!"
+    end
 end
--- db/migrate/20171005234856_create_courses.rb
+++ db/migrate/20171005234856_create_courses.rb
@@ -1,7 +1,9 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
-
+      t.string :course_name
+      t.integer :teacher_id
+      t.array :student_list
       t.timestamps null: false
     end
   end
