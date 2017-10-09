ff6c828c816d4bc9f4f7840948b621300adeaee0
Hana Pearlson <hpearlso@tulane.edu>
2017-10-09 22:37:59 +0000

    testing 222

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-course1 = Course.create(course_name: "Intro", teacher_id: 1, student_list: 1)
+course1 = Course.create([{ course_name: "Intro"}, { teacher_id: 1 }, { student_list: 1} ])
