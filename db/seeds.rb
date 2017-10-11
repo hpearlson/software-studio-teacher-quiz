352072102feac004d7079341e7345e4c71615da4
msummer300 <msummer@tulane.edu>
2017-10-11 21:22:39 +0000

    Seed file contains stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+courses = [
+    {:course_name => 'Intro to Computer Science', :teacher_id => 1, :student_list => 1},
+    {:course_name => 'Software Studio', :teacher_id => 2, :student_list => 2}
+]
 
-    
+courses.each do |course|
+    Course.create!(course)
+end
