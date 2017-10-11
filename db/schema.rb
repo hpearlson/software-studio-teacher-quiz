1f82e69041091b41934e5d17ca54139614b8a57f
msummer300 <msummer@tulane.edu>
2017-10-11 21:14:29 +0000

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20171005234856) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string   "course_name"
     t.integer  "teacher_id"
