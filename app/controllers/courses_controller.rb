870397e3a99c5de2f0ca09f9b6ecda9fd3f8d289
msummer300 <msummer@tulane.edu>
2017-10-16 20:31:42 +0000

    implementing new

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,20 @@ class CoursesController < ApplicationController
     end
     
     def show
-      # @course = Course.find(params[:id])
+       @course = Course.find(params[:id])
+    end
+    
+    def new
+       @couse = Course.new 
+    end
+    
+    def create
+        @course = Course.create!(course_params)
+        flash[:notice] = "#{@course.title} was successfully created."
+        redirect_to courses_path
+    end
+    
+    def course_params
+        params.require(:course).permit(:course_name, :teacher_id, :student_list)
     end
 end
