fe8a0621d0aab5a7d938adf786af5b14d2a411e0
msummer300 <msummer@tulane.edu>
2017-10-17 20:23:30 +0000

    implemented update and destroy for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,6 +17,26 @@ class CoursesController < ApplicationController
         redirect_to courses_path
     end
     
+    def edit
+       @course = Course.find params[:id] 
+    end
+    
+    def update
+       @course = Course.find params[:id]
+       @course.update_attributes!(course_params)
+       flash[:notice] = "#{@course.course_name} was successfully updated."
+       redirect_to course_path(@course)
+    end
+    
+    def destroy
+        @course = Course.find params[:id]
+        @course.destroy
+        flash[:notice] = "#{@course.course_name} was successfully deleted"
+        redirect_to courses_path
+    end
+    
+    private
+    
     def course_params
         params.require(:course).permit(:course_name, :teacher_id, :student_list)
     end
