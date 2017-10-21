5c11140f82ffe8270a9113a4a7574f0ee8bcaf71
msummer300 <msummer@tulane.edu>
2017-10-21 18:42:13 +0000

    fixed printing array problem

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,7 +7,7 @@ class CoursesController < ApplicationController
     
 	def show 
   	    @course = Course.find(params[:id]) 
-  	    @students = @course.students
+  	    @students = Student.all.where(:course => @course)
 	end
     
     def new
