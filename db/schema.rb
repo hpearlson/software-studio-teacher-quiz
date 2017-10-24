ccbca41962312bbc6ca375cfc1fc921a05024163
Walter Ogozaly <walter97ogozaly@gmail.com>
2017-10-24 01:10:25 +0000

    Migration file for teacher's password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024000951) do
+ActiveRecord::Schema.define(version: 20171024010757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171024000951) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
