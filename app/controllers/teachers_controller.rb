8185bd359e4419d2360d51903470f3364a08521a
Walter Ogozaly <walter97ogozaly@gmail.com>
2017-10-24 05:21:41 +0000

    Now able to create teachers with login info

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -6,8 +6,16 @@ class TeachersController < ApplicationController
     
     def create
         @teacher = Teacher.create!(teacher_params)
+        #if @teacher.save
         flash[:notice] = "#{@teacher.first_name} was successfully created."
         redirect_to teachers_path
+        #else
+        #    redirect_to '/home'
+        #end
+    end
+    
+    def password_required?
+        false
     end
     
     def new
@@ -17,6 +25,6 @@ class TeachersController < ApplicationController
     private
     
     def teacher_params
-        params.require(:teacher).permit(:username, :first_name, :last_name, :description, :image, :password_digest)
+        params.require(:teacher).permit(:username, :first_name, :last_name, :description, :image, :password_digest, :password)
     end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,4 @@
 class Teacher < ActiveRecord::Base
     has_many :courses
-    
     has_secure_password
 end
