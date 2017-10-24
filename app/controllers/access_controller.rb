c4b30cba0ce88a81982c44727af4eaf4df4c6d1a
Walter Ogozaly <walter97ogozaly@gmail.com>
2017-10-24 03:49:27 +0000

    Access controller, login page skeleton

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,17 @@
+class AccessController < ApplicationController
+
+  def menu
+    # display text & links
+  end
+
+  def login
+    # login form
+  end
+  
+  def attempt_login
+  end
+  
+  def logout
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :courses
+  
   root :to => redirect('/courses')
+  
+  get 'admin', :to => 'access#menu'
+  get 'access/menu'
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
+
+  resources :courses
   
   resources :students
   #get 'student/index' => 'student#index'
