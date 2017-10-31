ae3b5d9f0443391b168211720c93f4ab571e5236
Walter Ogozaly <walter97ogozaly@gmail.com>
2017-10-31 02:31:09 +0000

    Better session tracking with usernames

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -25,6 +25,7 @@ class AccessController < ApplicationController
   
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
       flash[:notice] = "You are now logged in, " + authorized_user.username
       redirect_to("/courses")
     else
@@ -36,6 +37,7 @@ class AccessController < ApplicationController
   
   def logout
     session[:user_id] = nil
+    session[:username] = nil
     flash[:notice] = 'Logged out'
     redirect_to('/home')
   end
