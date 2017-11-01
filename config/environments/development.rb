b67d6d28050f314da523014b275cc9c7d4c49fb3
Walter Ogozaly <walter97ogozaly@gmail.com>
2017-11-01 21:00:30 +0000

    Got images working locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
+  #just added this
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 
 end
